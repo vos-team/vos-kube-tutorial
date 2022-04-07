ddaefdff5fc4aae47a599f2a31e9ea49076a7269
nanangqq <gingggg@gmail.com>
2022-04-07 11:48:25 +0900

    도커 cgroup 관리를 systemd 사용 설정 스크립트를 추가하라

---
+++ set_docker_systemd.sh
@@ -0,0 +1,13 @@
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker

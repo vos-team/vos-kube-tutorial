acf2cf27e3983ac675bd9b9322df67745774fa04
nanangqq <gingggg@gmail.com>
2022-04-07 11:40:37 +0900

    유저이름 환경변수 호출 스크립트 수정하라

--- install_docker.sh
+++ install_docker.sh
@@ -4,4 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker $(USER)
+sudo usermod -aG docker $USER

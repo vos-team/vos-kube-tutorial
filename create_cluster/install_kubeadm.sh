1ddada354eafb92326394ab6a9ba276cd89bd012
nanangqq <gingggg@gmail.com>
2022-05-20 18:01:42 +0900

    kubadm 버전고정 취소하라

--- create_cluster/install_kubeadm.sh
+++ create_cluster/install_kubeadm.sh
@@ -9,5 +9,5 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm=1.23.5 kubectl
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
